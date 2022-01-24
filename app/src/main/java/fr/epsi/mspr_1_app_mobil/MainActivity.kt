d9c3000bbef143a5adb2b3a2772a098fc522e32c
oualid3 <73546426+oualid3@users.noreply.github.com>
2022-01-24 16:33:26 +0100

    Project file

---
+++ app/src/main/java/fr/epsi/mspr_1_app_mobil/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.epsi.mspr_1_app_mobil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
