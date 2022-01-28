84335696b0d48d0d696d1f9d6fd0aace5a206e2a
sofianeoa <95252867+sofianeoa@users.noreply.github.com>
2022-01-28 15:57:33 +0100

    git de tout

--- app/src/main/java/fr/epsi/mspr_1_app_mobil/LoginActivity.kt
+++ app/src/main/java/fr/epsi/mspr_1_app_mobil/LoginActivity.kt
@@ -3,7 +3,6 @@ package fr.epsi.mspr_1_app_mobil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import fr.epsi.mspr_1_app_mobil.R
 import android.widget.TextView
 import com.google.android.material.button.MaterialButton
 import android.widget.Toast
@@ -11,7 +10,7 @@ import android.widget.Toast
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         val username = findViewById<View>(R.id.username) as TextView
         val password = findViewById<View>(R.id.password) as TextView
         val loginbtn = findViewById<View>(R.id.loginbtn) as MaterialButton
